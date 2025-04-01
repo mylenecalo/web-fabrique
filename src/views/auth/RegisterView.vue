c17e6027a5cd6381969a836767c4ded7d07b3b50
mylenecalo <mylene.calo@carsu.edu.ph>
2025-04-01 16:32:22 +0800

    feat: addded login and register page

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -9,7 +9,7 @@ function onClick() {
 </script>
 
 <template>
-  <v-responsive class="border rounded">
+ <v-responsive class="border rounded">
     <v-app :theme="theme">
       <v-app-bar class="px-3" color="grey-lighten-1">
         <v-spacer></v-spacer>
@@ -26,13 +26,21 @@ function onClick() {
         <v-container>
           <v-row>
             <v-col cols="12" md="6" class="mx-auto">
-              <v-card class="mx-auto" prepend-icon="mdi-login" subtitle="Login" elevation="24">
+              <v-card class="mx-auto" prepend-icon="mdi-account-plus" subtitle="Register" elevation="24">
                 <template v-slot:title>
                   <span class="font-weight-black">Fabrique</span>
                 </template>
 
                 <v-card-text class="bg-surface-light pt-4">
                   <v-form fast-fail @submit.prevent>
+                    <v-text-field
+                      label="First Name" variant="outlined"
+                    ></v-text-field>
+
+                    <v-text-field
+                      label="Last Name" variant="outlined"
+                    ></v-text-field>
+
                     <v-text-field
                       label="Email" variant="outlined"
                     ></v-text-field>
@@ -41,12 +49,16 @@ function onClick() {
                       label="Password" type="password" variant="outlined"
                     ></v-text-field>
 
+                    <v-text-field
+                      label="Password Confirmation" type="password" variant="outlined"
+                    ></v-text-field>
+
                     <v-btn class="mt-2" type="submit" block>Submit</v-btn>
                   </v-form>
 
                   <v-divider class="my-5"></v-divider>
 
-                  <h5 class="text-center">Don't have an account? <RouterLink class="text-primary" to="/register">Click here to Register</RouterLink></h5>
+                  <h5 class="text-center">Already have account? <RouterLink class="text-primary" to="/">Click here to Login</RouterLink></h5>
                 </v-card-text>
               </v-card>
             </v-col>
